7933be4e52979bea954cc47791990e8b8c5cae8d
m0ai <frozenmoai@gmail.com>
2020-12-20 16:04:05 +0900

    Complete a skeleton slackbot ()

--- workers/scrapper/main.go
+++ workers/scrapper/main.go
@@ -10,29 +10,43 @@ import (
 	"time"
 )
 
-type Item []struct {
+type Item struct {
 	XMLName   xml.Name `xml:"item" json:"Item" db:""`
 	Seq       int `xml:"seq"`
-	DecideCnt int `xml:"decideCnt"`
-	DeathCnt  int `xml:"deathCnt"`
-	CareCnt   int `xml:"careCnt"`
+	DecideCnt int `xml:"decideCnt"` // 누적 확진자 수
+	DeathCnt  int `xml:"deathCnt"` // 사망자 수
+	CareCnt   int `xml:"careCnt"` // 치료중 환자 수
+	ClearCnt  int `xml:"clearCnt"` // 격리 해제 수
 	StateDt   string `xml:"stateDt"`
 	StateTime string `xml:"stateTime"`
 	CreateDt  string `xml:"createDt"`
+
+	TodayDecideCnt int
 }
 
-// Xml => response != Response
 type Response struct {
 	XMLName xml.Name `xml:"response"`
 	Items []Item `xml:"body>items>item"`
 }
 
-
 // Scrapping a Covid-19 Information
 // https://www.thepolyglotdeveloper.com/2017/03/parse-xml-data-in-a-golang-application/
-func Scrape(xmlFilePath string) {
-	t := time.Now()
-	getCovidDataFromAPI(t, t)
+func Scrape(xmlFilePath string) Item {
+	startDt := time.Now().AddDate(0,0, -1)
+	endDt := time.Now()
+	item := getCovidDataFromAPI(startDt, endDt)
+	// item := makeCovid19MockStruct()
+	return item
+}
+
+func makeCovid19MockStruct() *Item {
+	item := Item{
+		Seq:       1,
+		CareCnt:   3,
+		DeathCnt:  2,
+		DecideCnt: 2,
+	}
+	return &item
 }
 
 func requestTo(baseUrl string, params map[string]string) ([]byte, error) {
@@ -72,7 +86,7 @@ func requestTo(baseUrl string, params map[string]string) ([]byte, error) {
 // numOfRows=10
 // startCreateDt=20200310
 // endCreateDt=20200315
-func getCovidDataFromAPI(startDate, endDate time.Time) {
+func getCovidDataFromAPI(startDate, endDate time.Time) (extractedCovid19Data Item){
 	var baseUrl string = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson"
 	params := map[string]string{
 		"ServiceKey":    os.Getenv("OPEN_API_KEY"),
@@ -81,9 +95,9 @@ func getCovidDataFromAPI(startDate, endDate time.Time) {
 		"pageNo":        "1",
 		"numOfRows":     "10",
 	}
-
 	xmlData, _ := requestTo(baseUrl, params)
-	_ = extractData(xmlData)
+	extractedCovid19Data = extractFirstData(xmlData)
+	return
 }
 
 func extractData (data []byte) []Item {
@@ -97,5 +111,9 @@ func extractData (data []byte) []Item {
 		fmt.Println(item)
 	}
 	return res.Items
+}
+
+func extractFirstData (data []byte) Item {
+	return extractData(data)[0]
 }
 
